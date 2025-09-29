fbe3ddf9f9d2adcc93305d681a44c65a04147934
Arthuryh <ayhayakawa@gmail.com>
2025-09-28 21:55:57 -0300

    adicao de rotas, regras e validacoes de usuarios/roles

---
+++ Dominio/Entidades/Administrador.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinimalApi.Dominio.Entidades;
+
+public class Administrador
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public required string Email { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public required string Senha { get; set; }
+
+    [Required]
+    [StringLength(10)]
+    public required string Perfil { get; set; }
+}
